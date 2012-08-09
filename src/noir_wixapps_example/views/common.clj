9c7fb7b2553b0c058032b0c14ae888e9c900d674
Dimitri Krassovski <dimitri@wix.com>
2012-08-09 21:44:17 +0300

    Initial commit

---
+++ src/noir_wixapps_example/views/common.clj
@@ -0,0 +1,12 @@
+(ns noir-wixapps-example.views.common
+  (:use [noir.core :only [defpartial]]
+        [hiccup.page-helpers :only [include-css html5]]))
+
+(defpartial layout [& content]
+            (html5
+              [:head
+               [:title "noir-wixapps-example"]
+               (include-css "/css/reset.css")]
+              [:body
+               [:div#wrapper
+                content]]))
